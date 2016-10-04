aa52b01318ac47a84de4fca677c3228317f73046
Daniel <danielc.web@gmail.com>
2016-10-04 17:07:17 -0500

    Rest server finished

--- eps_server/eps_server/urls.py
+++ eps_server/eps_server/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 
 
 urlpatterns = [
-url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest/', include('rest_server.urls', namespace='rest')),
 
 ]
--- eps_server/rest_server/views.py
+++ eps_server/rest_server/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from rest_framework import mixins,viewsets
+from rest_framework import permissions
+from rest_server.models import EPS, EPSSerializable
 # Create your views here.
+
+
+class EPSListView(mixins.ListModelMixin,viewsets.GenericViewSet):
+    """
+    Vista encargada de listar las eps y obtenerlas por código mediante GET con query
+    """
+    queryset = EPS.objects.all()
+    serializer_class = EPSSerializable
+    permission_classes = (permissions.AllowAny, )
+
+    def list(self, request, *args, **kwargs):
+        if 'codigo' in request.query_params:
+            self.queryset = self.queryset.filter(codigo=request.query_params.get('codigo'))
+        return super(EPSListView, self).list(request, *args, **kwargs)
+
